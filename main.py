96323e853a02bf2431ccb427477d884d2cb05ea3
Keisuke Saito <keis0107.ytm@gmail.com>
2020-11-07 01:44:35 +0900

    modify

--- main.py
+++ main.py
@@ -6,62 +6,61 @@ import numpy as np
 import os
 from PIL import Image
 
-path = os.getcwd()
-print(path)
-#データセットの作成
 
-from torchvision.datasets import ImageFolder
-from data_augumentation import Compose, Resize, Scale, RandomRotation, RandomMirror
+def main():
+    #データセットの作成
+    from torchvision.datasets import ImageFolder
+    from data_augumentation import Compose, Resize, Scale, RandomRotation, RandomMirror
 
-width = 512
-height = 512
-mean = (0.5, 0.5, 0.5)
-std = (0.5, 0.5, 0.5)
-
-
-train_transform = transforms.Compose([
-    RandomRotation(angle=[-10,10]),
-    RandomMirror(),
-    Resize(width, height),
-    transforms.ToTensor(),
-    transforms.Normalize(mean, std)
-])
-
-val_transform = transforms.Compose([
-    Resize(width, height),
-    transforms.ToTensor(),
-    transforms.Normalize(mean, std)
-])
+    width = 512
+    height = 512
+    mean = (0.5, 0.5, 0.5)
+    std = (0.5, 0.5, 0.5)
 
 
-train_images=ImageFolder(
-    '/kw_resources/Img_classification/data/train/',
-    transform=train_transform
-)
+    train_transform = transforms.Compose([
+        RandomRotation(angle=[-10,10]),
+        RandomMirror(),
+        Resize(width, height),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
 
-test_images=ImageFolder(
-    '/kw_resources/Img_classification/data/val/',
-    transform=val_transform
-)
-
-train_loader=torch.utils.data.DataLoader(train_images,batch_size=12,shuffle=True, num_workers=2)
-test_loader=torch.utils.data.DataLoader(test_images,batch_size=12,shuffle=True, num_workers=2)
-
-
-from model import ResNet
-in_ch = 3
-f_out = 32
-n_ch = 37
-
-model = ResNet(in_ch, f_out, n_ch)
+    val_transform = transforms.Compose([
+        Resize(width, height),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
 
 
+    train_images=ImageFolder(
+        '/kw_resources/Img_classification/data/train/',
+        transform=train_transform
+    )
 
-#モデルの学習
-from train import train
-num_epoch = 300
+    test_images=ImageFolder(
+        '/kw_resources/Img_classification/data/val/',
+        transform=val_transform
+    )
 
-up_model = train(model, num_epoch, train_loader, test_loader)
+    train_loader=torch.utils.data.DataLoader(train_images,batch_size=12,shuffle=True, num_workers=2)
+    test_loader=torch.utils.data.DataLoader(test_images,batch_size=12,shuffle=True, num_workers=2)
+
+
+    from model import ResNet
+    in_ch = 3
+    f_out = 32
+    n_ch = 37
+
+    model = ResNet(in_ch, f_out, n_ch)
+
+
+
+    #モデルの学習
+    from train import train
+    num_epoch = 300
+
+    up_model = train(model, num_epoch, train_loader, test_loader)
 
 
 
@@ -69,7 +68,8 @@ print('finish')
 
 
 
-
+if __name__ == '__main__':
+    main()
 
 
 
